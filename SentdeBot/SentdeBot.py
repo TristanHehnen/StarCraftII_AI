5e2b7dc32c76ce67fa5ce5aba498ac1aee2a16a7
unknown <tristan.hehnen@cern.ch>
2018-08-03 21:25:47 +0200

    SentdeBot has now primitive attacking capabilities, from pt. 5 of the tutoial. ThePurpleMenace was a bit fleshed out and has primitive offensive capabilities, as well

--- SentdeBot/SentdeBot.py
+++ SentdeBot/SentdeBot.py
@@ -4,6 +4,7 @@ https://www.youtube.com/watch?v=v3LJ6VvpfgI&t=1s
 """
 
 import sc2
+import random
 # print(sc2.__file__)
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
@@ -22,6 +23,7 @@ class SentdeBot(sc2.BotAI):
         await self.expand()
         await self.offensive_force_buildings()
         await self.build_offensive_force()
+        await self.attack()
 
     # Build new workers at a nexus that is ready.
     async def build_workers(self):
@@ -37,19 +39,19 @@ class SentdeBot(sc2.BotAI):
                 if self.can_afford(PYLON):
                     await self.build(PYLON, near=nexuses.first)
 
-    # Look for vaspene geysers close to nexuses and build a refinery on top
+    # Look for vespene geysers close to nexuses and build a refinery on top
     # them, if one can afford it.
     async def build_assimilators(self):
         for nexus in self.units(NEXUS).ready:
-            vaspenes = self.state.vespene_geyser.closer_than(15.0, nexus)
-            for vaspene in vaspenes:
+            vespenes = self.state.vespene_geyser.closer_than(15.0, nexus)
+            for vespene in vespenes:
                 if not self.can_afford(ASSIMILATOR):
                     break
-                worker = self.select_build_worker(vaspene.position)
+                worker = self.select_build_worker(vespene.position)
                 if worker is None:
                     break
-                if not self.units(ASSIMILATOR).closer_than(1.0, vaspene).exists:
-                    await self.do(worker.build(ASSIMILATOR, vaspene))
+                if not self.units(ASSIMILATOR).closer_than(1.0, vespene).exists:
+                    await self.do(worker.build(ASSIMILATOR, vespene))
 
     # Expand to a new location with resources.
     async def expand(self):
@@ -59,10 +61,10 @@ class SentdeBot(sc2.BotAI):
     #
     async def offensive_force_buildings(self):
         # Find random construction site near random pylon.
-        if self.units(PYLON).ready:#.exits:
+        if self.units(PYLON).ready:
             pylon = self.units(PYLON).ready.random
             # Figure out if a gateway exists.
-            if self.units(GATEWAY).ready:#.exists:
+            if self.units(GATEWAY).ready:
                 # Figure out if a cybernetics core exists. If not then try to
                 #  build one.
                 if not self.units(CYBERNETICSCORE):
@@ -80,6 +82,15 @@ class SentdeBot(sc2.BotAI):
         for gw in self.units(GATEWAY).ready.noqueue:
             if self.can_afford(STALKER) and self.supply_left > 0:
                 await self.do(gw.train(STALKER))
+
+    # Attack known enemy units.
+    async def attack(self):
+        if self.units(STALKER).amount > 5:
+            if self.known_enemy_units:
+                for s in self.units(STALKER).idle:
+                    await self.do(s.attack(random.choice(
+                        self.known_enemy_units)))
+
 
 # Starts the game. Choose the map and the participants, race and difficulty.
 run_game(maps.get("AbyssalReefLE"),
--- ThePurpleMenace/ThePurpleMenace.py
+++ ThePurpleMenace/ThePurpleMenace.py
@@ -1,14 +1,106 @@
 import sc2
+import random
+# print(sc2.__file__)
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
+from sc2.constants import HATCHERY, EXTRACTOR, SPAWNINGPOOL, LARVA, OVERLORD, \
+    DRONE, ZERGLING
 
 
 # The Purple Menace -- a Zerg hive mind,
-# about to cover the wold in creep and gore.
+# about to cover the world in creep and gore.
 class ThePurpleMenace(sc2.BotAI):
     async def on_step(self, iteration):
         # Limit the workers to as much a three per resource patch.
         await self.distribute_workers()
+        await self.spawn_overlord()
+        await self.morph_drone()
+        await self.build_extractor()
+        await self.build_spawningpool()
+        await self.expand()
+        await self.morph_zergling()
+        await self.attack()
+
+    # Spawn new overlord when necessary.
+    async def spawn_overlord(self):
+        if self.supply_left < 5 and not self.already_pending(OVERLORD):
+            if self.can_afford(OVERLORD) and self.units(LARVA).ready.noqueue:
+                larva = self.units(LARVA).random
+                await self.do(larva.train(OVERLORD))
+
+            # for larva in self.units(LARVA).ready.noqueue:
+            #     if self.can_afford(OVERLORD):
+            #         await self.do(larva.train(OVERLORD))#, near=nexuses.first)
+
+    # Morph more drones.
+    async def morph_drone(self):
+        hatcheries = self.units(HATCHERY).amount * 16
+        # Use only two drones per geyser, because they get removed from the
+        # game while "inside" the extractor. Thus, the count will be off
+        # occasionally. However, eventually extra drones get spawned to fill
+        # up the difference.
+        extractors = self.units(EXTRACTOR).amount * 2
+        # Account for drones in the process of being morphed.
+        drones = self.units(DRONE).ready.amount + self.units(
+            DRONE).not_ready.amount
+        # print("* hatcheries: {}\n* extractors{}\n* drones{}".format(
+        #     hatcheries, extractors, drones))
+        drones_desired = hatcheries + extractors
+        if drones < drones_desired and \
+                self.units(LARVA).ready and self.can_afford(DRONE) \
+                and not self.already_pending(DRONE):
+            larva = self.units(LARVA).first
+            await self.do(larva.train(DRONE))
+
+    # Locate hatcheries and create vespene extractors in their vicinity.
+    async def build_extractor(self):
+        for hatchery in self.units(HATCHERY).ready:
+            geysers = self.state.vespene_geyser.closer_than(15.0, hatchery)
+            for geyser in geysers:
+                if not self.can_afford(EXTRACTOR):
+                    break
+                worker = self.select_build_worker(geyser.position)
+                if worker is None:
+                    break
+                if not self.units(EXTRACTOR).closer_than(1.0, geyser).exists \
+                        and not self.already_pending(EXTRACTOR):
+                    await self.do(worker.build(EXTRACTOR, geyser))
+
+    #
+    async def build_spawningpool(self):
+        if self.units(SPAWNINGPOOL).amount < 1 and \
+            self.can_afford(SPAWNINGPOOL) \
+                and not self.already_pending(SPAWNINGPOOL):
+                hatcheries = self.units(HATCHERY).ready
+                if hatcheries.exists:
+                    await self.build(SPAWNINGPOOL, near=hatcheries.first)
+
+    # Expand to a new location with resources.
+    async def expand(self):
+        if self.units(HATCHERY).amount < 3 and self.can_afford(HATCHERY):
+            await self.expand_now()
+
+    #
+    async def morph_zergling(self):
+        zerglings_desired = 50
+        # Account for zerglings in the process of being morphed.
+        zerglings = self.units(ZERGLING).ready.amount + self.units(
+            DRONE).not_ready.amount
+
+        if zerglings < zerglings_desired and \
+                self.units(LARVA).ready and self.can_afford(ZERGLING) \
+                and not self.already_pending(ZERGLING):
+            larva = self.units(LARVA).first
+            await self.do(larva.train(ZERGLING))
+
+    # Attack known enemy units.
+    async def attack(self):
+        if self.units(ZERGLING).amount > 5:
+            if self.known_enemy_units:
+                for s in self.units(ZERGLING).idle:
+                    await self.do(s.attack(random.choice(
+                        self.known_enemy_units)))
+
 
 
 # Starts the game. Choose the map and the participants, race and difficulty.
